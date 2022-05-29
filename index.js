3e8de39be645c86621443db51adf4c021e75f41c
mdsujon-mridha <mdsujan134523@gmail.com>
2022-05-29 13:38:42 +0600

    delet product by admin

--- index.js
+++ index.js
@@ -214,6 +214,13 @@ async function run() {
             const result = await bookingCollection.deleteOne(filter);
             res.send(result);
         });
+        // delete product by admin 
+        app.delete('/products/:id',async(req,res)=>{
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await productsCollection.deleteOne(filter);
+            res.send(result);
+        })
 
     }
     finally {
